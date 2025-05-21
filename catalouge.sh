3d49715161b39ad3fffee172c4b68d4da2ebda9f
Divyani <divyani.t74@gmail.com>
2025-05-21 14:04:05 +0530

    roboshop

--- catalouge.sh
+++ catalouge.sh
@@ -78,10 +78,10 @@ cp $SCRIPT_DIR/mongodb.repo /etc/yum.repos.d/mongodb.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
-STATUS=$(mongosh --host mongodb.devsecops.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+STATUS=$(mongosh --host mongodb.devsecops-dt.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
-    mongosh --host mongodb.devsecops.site </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host mongodb.devsecops-dt.site </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
